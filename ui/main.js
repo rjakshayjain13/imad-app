fbbefb817d24b4f597a24968a880f9aeecd198c0
Akshay Jain <rj.akshayjain@yahoo.com>
2017-09-03 12:08:24 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,3 +1,10 @@
+var button = document.getElementById('counter');
+var counter = 0;
+button.onclick = function(){
+    counter = counter+1;
+    var span = document.getElementById('count');
+    span.innerHTML = counter.toString();
+};
 /*var button = document.getElementById('counter');
 
 button.onclick = function () {
@@ -17,12 +24,3 @@ button.onclick = function () {
     request.open('GET', 'http://ajain122.imad.hasura-app.io/counter', true);
     request.send(null);
 };*/
-
-var button = document.getElementById('counter');
-var counter = 0;
-
-button.onclick = function () {
-    counter = counter + 1;
-    var span = document.getElementById('count');
-    span.innerHTML = counter.toString();
-};
