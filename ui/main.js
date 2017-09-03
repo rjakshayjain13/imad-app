5c1a343a93b663a84fc3650d9cb0aec93e34fe62
Akshay Jain <rj.akshayjain@yahoo.com>
2017-09-03 15:06:14 -0700

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,11 +1,11 @@
-var button = document.getElementById('counter');
+/*var button = document.getElementById('counter');
 var counter = 0;
 button.onclick = function(){
     counter = counter+1;
     var span = document.getElementById('count');
     span.innerHTML = counter.toString();
-};
-/*var button = document.getElementById('counter');
+};*/
+var button = document.getElementById('counter');
 
 button.onclick = function () {
     
@@ -23,4 +23,18 @@ button.onclick = function () {
     };
     request.open('GET', 'http://ajain122.imad.hasura-app.io/counter', true);
     request.send(null);
-};*/
+};
+
+//submit name
+var nameInput =document.getElementById('name');
+var name = nameInput.value;
+var submit = document.getElementById('submit_btn');
+submit.onclick=function(){
+  var names = ['name1', 'name2', 'name3', 'name4'];
+  var list = '';
+  for(var i=0; i<names.length; i++){
+      list += '<li>' +names[i]+ '</li>';
+  }
+  var ul = document.getElementById('namelist');
+  ul.innerHTML - list;
+};
